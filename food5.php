8d66845cd33f4bef91b5e46216acab04cb7e802c
kittisak191 <97100702+kittisak191@users.noreply.github.com>
2023-05-22 14:12:39 +0700

    first commit

--- food5.php
+++ food5.php
@@ -10,7 +10,7 @@ $rows = mysqli_num_rows($result_food);
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+  <link href="https://getbootstrap.com/docs/git branch -M main5.3/assets/css/docs.css" rel="stylesheet">
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/swiper.min.css">
   <!-- <link rel="stylesheet" href="css/responsive.css"> -->
